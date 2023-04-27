981e4ff5fdd2df91e4a98ebec11fdf5eb360a939
yogee-chintu <yogeeshwarm@brightcomgroup.com>
2023-04-27 10:51:31 +0000

    reac

---
+++ React_Immutable/6-main.js
@@ -0,0 +1,23 @@
+import mergeDeeplyElements from './6-deeply';
+
+const page1 = {
+  'user-1': {
+    id: 1,
+    name: 'test',
+    likes: {
+      1: {
+        uid: 1234,
+      },
+    },
+  },
+};
+
+const page2 = {
+  'user-1': {
+    likes: {
+      2: {
+        uid: 134,
+      },
+    },
+  },
+};
